d94ffcd2a90c47fbaf117988b93d7216ed0a6c33
Ubuntu <ubuntu@ip-172-31-0-21.ec2.internal>
2024-04-11 20:19:30 +0000

    Commit1

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+import socket
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Welcome to Samruddi Farms'
+
+@app.route('/name')
+def name():
+    return 'This is name'
+
+@app.route('/ip')
+def ip():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
 if __name__ == '__main__':
-  app.run()
+    app.run()
